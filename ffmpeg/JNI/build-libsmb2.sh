b048e4167465c6b8b419c037d44c3c10e99919e7
khirendra <khirendrapatel3@gmail.com>
2025-05-06 19:15:26 +0530

    1.90.1: files
    Signed-off-by: khirendra <khirendrapatel3@gmail.com>

--- ffmpeg/JNI/build-libsmb2.sh
+++ ffmpeg/JNI/build-libsmb2.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-
-. ENV # Environment
-
+#!/bin/bash 
 ROOT=$(cd "$(dirname "$0")"; pwd)
 source ${ROOT}/util.sh
 
-cd ${ROOT}/openssl-1.0.2s
+cd ${ROOT}/libsmb2
 
 if test -z ${NDK}
 then
@@ -25,27 +22,27 @@ TOOLCHAIN=${NDK}/toolchains/llvm/prebuilt/${HOST_PLATFORM}
 if [ ${FLAVOR} == 'neon' ] || [ ${FLAVOR} == 'tegra2' ] || [ ${FLAVOR} == 'tegra3' ]
 then
     CROSS_PREFIX=${TOOLCHAIN}/bin/arm-linux-androideabi-
-    TARGET=android-armv7
     CLANG_TARGET="armv7a-none-linux-androideabi21"
+    HOST="arm-linux-android"
 
 elif [ ${FLAVOR} == 'arm64' ] 
 then
     CROSS_PREFIX=${TOOLCHAIN}/bin/aarch64-linux-android-
-    TARGET=android64-aarch64
     TRIPLE=aarch64-linux-android
     CLANG_TARGET="aarch64-none-linux-android21" 
+    HOST="aarch64-linux-android"
 
 elif [ ${FLAVOR} == 'x86' ] 
 then
     CROSS_PREFIX=${TOOLCHAIN}/bin/i686-linux-android-
-    TARGET=android-x86
     CLANG_TARGET="i686-none-linux-android21"
+    HOST="x86-linux-android"
 
 elif [ ${FLAVOR} == 'x86_64' ] 
 then
     CROSS_PREFIX=${TOOLCHAIN}/bin/x86_64-linux-android-
-    TARGET=android
     CLANG_TARGET="x86_64-none-linux-android21"
+    HOST="x86_64-linux-android"
 else
     die "Unsupported architecture."
 fi
@@ -54,29 +51,36 @@ if [ ! -e ${CROSS_PREFIX}ar ]; then
   CROSS_PREFIX=${TOOLCHAIN}/bin/llvm-
   fi
 
-export ANDROID_SYSROOT=$NDK/toolchains/llvm/prebuilt/$HOST_PLATFORM/sysroot
+export SYSROOT=$NDK/toolchains/llvm/prebuilt/$HOST_PLATFORM/sysroot
 export CC="$NDK/toolchains/llvm/prebuilt/$HOST_PLATFORM/bin/clang -target $CLANG_TARGET"
 export CXX="$NDK/toolchains/llvm/prebuilt/$HOST_PLATFORM/bin/clang++ -target $CLANG_TARGET"
 export RANLIB=${CROSS_PREFIX}ranlib
 export AR=${CROSS_PREFIX}ar
+export CPPFLAGS="-DMXTECHS"
+export LDFLAGS="-Wl,-z,max-page-size=16384"
 
-./Configure ${TARGET}
+./bootstrap
+./configure --enable-static=no      \
+            --disable-werror        \
+            --host=$HOST            \
+            --without-libkrb5       \
+            --with-pic              \
+            --with-sysroot=$SYSROOT
+
 if test "$?" != 0; then
-    die "ERROR: failed to configure openssl.${FLAVOR}"
+    die "ERROR: failed to configure libsmb2.${FLAVOR}"
 fi
 
-echo "BUILDING openssl.${FLAVOR}"
-make clean; make build_libs -j 4
+echo "BUILDING libsmb2.${FLAVOR}"
+make clean; make -j 4
 if test "$?" != 0; then
-    die "ERROR: failed to build openssl.${FLAVOR}"
+    die "ERROR: failed to build libsmb2.${FLAVOR}"
 fi
-#<<!
+
 DST_DIR=$(get_dst_dir ${FLAVOR})
 echo "COPYING openssl.${FLAVOR} to ${DST_DIR}"
 if ! test -d  ${DST_DIR}
 then
     mkdir -p ${ROOT}/${DST_DIR}
 fi
-cp libssl.a ${ROOT}/${DST_DIR}
-cp libcrypto.a ${ROOT}/${DST_DIR}
-#!
+cp lib/.libs/libsmb2.so ${ROOT}/${DST_DIR}
